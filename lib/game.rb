c42e04512dade90dfae983d55b9e206135e0315a
clairedotcom <claireacarson@gmail.com>
2021-06-11 12:51:17 -0400

    Added to Game class

--- lib/game.rb
+++ lib/game.rb
@@ -1,7 +1,20 @@
 class Game
     def initialize
         @board = Board.new
-        @computer_player = nil
-        @human_player = nil
-    end    
+        @computer_player = Player.new("human","X")
+        @human_player = Player.new("computer","O")
+    end
+    
+    def randomize_first_player
+        number = rand(1)
+        if number == 1
+            player_one = computer_player
+        else
+            player_one = human_player
+        end    
+    end
+    
+
+    
+
 end 
--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -3,7 +3,11 @@ require_relative "board.rb"
 require_relative "game.rb"
 require_relative "dialogue.rb"
 
+include Dialogue
+
+puts greeting
 test = Board.new
 test.display_board
-puts greeting
+puts randomize_first_player
+
 
