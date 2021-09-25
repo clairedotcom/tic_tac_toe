1bf6701ed991e4fd3db095436938f6c5a6f6d19c
clairedotcom <claireacarson@gmail.com>
2021-09-25 17:01:17 -0400

    Remove extra files and rename tic_tac_toe.rb to main.rb

--- lib/board.rb
+++ lib/board.rb
@@ -3,8 +3,8 @@ class Board
 
     attr_accessor :grid
     
-    def initialize
-        @grid = [1,2,3,4,5,6,7,8,9]
+    def initialize(grid = [1,2,3,4,5,6,7,8,9])
+        @grid = grid
     end
 
     def display_board
